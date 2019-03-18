8a7d398bea3ec9eca72f28b4280e91b620ae58bf
Cam Arrigo <carrigo3@gmail.com>
2019-03-18 03:04:31 -0400

    7.16 Completed

--- WeatherGift/DetailVC.swift
+++ WeatherGift/DetailVC.swift
@@ -24,8 +24,11 @@ class DetailVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        locationLabel.text = locationsArray[currentPage].name
-        dateLabel.text = locationsArray[currentPage].coordinates
+        if currentPage != 0 {
+            self.locationsArray[currentPage].getWeather {
+                self.updateUserInterface()
+            }
+        }
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -38,6 +41,7 @@ class DetailVC: UIViewController {
     func updateUserInterface() {
         locationLabel.text = locationsArray[currentPage].name
         dateLabel.text = locationsArray[currentPage].coordinates
+        temperatureLabel.text = locationsArray[currentPage].currentTemp
     }
 }
 extension DetailVC: CLLocationManagerDelegate {
@@ -45,8 +49,6 @@ extension DetailVC: CLLocationManagerDelegate {
     func getLocation(){
         locationManager = CLLocationManager()
         locationManager.delegate = self
-        let status = CLLocationManager.authorizationStatus()
-        handleLocationAuthorizationStatus(status: status)
     }
     
     func handleLocationAuthorizationStatus(status: CLAuthorizationStatus) {
@@ -73,7 +75,6 @@ extension DetailVC: CLLocationManagerDelegate {
         let currentLatitude = currentLocation.coordinate.latitude
         let currentLongitude = currentLocation.coordinate.longitude
         let currentCoordinates = "\(currentLatitude),\(currentLongitude)"
-        print(currentCoordinates)
         dateLabel.text = currentCoordinates
         geoCoder.reverseGeocodeLocation(currentLocation, completionHandler:
             {placemarks, error in
@@ -86,8 +87,9 @@ extension DetailVC: CLLocationManagerDelegate {
                 }
                 self.locationsArray[0].name = place
                 self.locationsArray[0].coordinates = currentCoordinates
-                self.locationsArray[0].getWeather()
-                self.updateUserInterface()
+                self.locationsArray[0].getWeather {
+                    self.updateUserInterface()
+                }
         })
     }
     
