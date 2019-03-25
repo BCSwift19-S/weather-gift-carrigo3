ecba2cda230f39c3c6aed5486ff3b90443d0b494
Cam Arrigo <carrigo3@gmail.com>
2019-03-25 01:25:24 -0400

    2.20 Completed

---
+++ WeatherGift/DayWeatherCell.swift
@@ -0,0 +1,29 @@
+//
+//  DayWeatherCell.swift
+//  WeatherGift
+//
+//  Created by Cameron Arrigo on 3/25/19.
+//  Copyright © 2019 Cameron Arrigo. All rights reserved.
+//
+
+import UIKit
+
+class DayWeatherCell: UITableViewCell {
+    @IBOutlet weak var dayCellIcon: UIImageView!
+    @IBOutlet weak var dayCellWeekday: UILabel!
+    @IBOutlet weak var dayCellMaxTemp: UILabel!
+    @IBOutlet weak var dayCellMinTemp: UILabel!
+    @IBOutlet weak var dayCellSummary: UITextView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
